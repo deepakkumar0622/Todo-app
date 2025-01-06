8795eb0aa0c3fe89640ba6c4efc37347dc4ebd2a
deepakkumar0622 <deepakramu456@gmail.com>
2025-01-06 11:03:58 +0530

    Todo-app Completed

--- src/Component/TodoList.js
+++ src/Component/TodoList.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './style.css';
-import todoImage from "../image/todo.png"
+import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./style.css";
+import todoImage from "../image/todo.png";
 
 const TodoList = () => {
   // State variables
   const [tasks, setTasks] = useState([]); // Holds the list of tasks
-  const [inputValue, setInputValue] = useState(''); // Holds the value of the input field
-  const [filter, setFilter] = useState('all'); // Holds the current filter type
+  const [inputValue, setInputValue] = useState(""); // Holds the value of the input field
+  const [filter, setFilter] = useState("all"); // Holds the current filter type
   const [isLoading, setIsLoading] = useState(true); // Indicates whether the data is being loaded
   const [editTaskId, setEditTaskId] = useState(null); // Holds the ID of the task being edited
 
@@ -20,12 +20,14 @@ const TodoList = () => {
   // Fetch todos from an API
   const fetchTodos = async () => {
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=4');
+      const response = await fetch(
+        "https://run.mocky.io/v3/15c7cfc8-7fe5-4af2-8eeb-4f69349147c4"
+      );
       const todos = await response.json();
       setTasks(todos);
       setIsLoading(false);
     } catch (error) {
-      console.log('Error fetching todos:', error);
+      console.log("Error fetching todos:", error);
       setIsLoading(false);
     }
   };
@@ -37,30 +39,33 @@ const TodoList = () => {
 
   // Add a new task
   const handleAddTask = async () => {
-    if (inputValue.trim() === '') {
+    if (inputValue.trim() === "") {
       return;
     }
 
     const newTask = {
       title: inputValue,
-      completed: false
+      completed: false,
     };
 
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
-        method: 'POST',
-        body: JSON.stringify(newTask),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      });
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos",
+        {
+          method: "POST",
+          body: JSON.stringify(newTask),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
       const addedTask = await response.json();
       setTasks((prevTasks) => [...prevTasks, addedTask]);
-      setInputValue('');
-      toast.success('Task added successfully');
+      setInputValue("");
+      toast.success("Task added successfully");
     } catch (error) {
-      console.log('Error adding task:', error);
-      toast.error('Error adding task');
+      console.log("Error adding task:", error);
+      toast.error("Error adding task");
     }
   };
 
@@ -76,7 +81,7 @@ const TodoList = () => {
   // Delete a task
   const handleDeleteTask = (taskId) => {
     setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
-    toast.success('Task deleted successfully');
+    toast.success("Task deleted successfully");
   };
 
   // Edit a task
@@ -88,35 +93,40 @@ const TodoList = () => {
 
   // Update a task
   const handleUpdateTask = async () => {
-    if (inputValue.trim() === '') {
+    if (inputValue.trim() === "") {
       return;
     }
 
     const updatedTask = {
       title: inputValue,
-      completed: false
+      completed: false,
     };
 
     try {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${editTaskId}`, {
-        method: 'PUT',
-        body: JSON.stringify(updatedTask),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      });
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${editTaskId}`,
+        {
+          method: "PUT",
+          body: JSON.stringify(updatedTask),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
       const updatedTaskData = await response.json();
       setTasks((prevTasks) =>
         prevTasks.map((task) =>
-          task.id === editTaskId ? { ...task, title: updatedTaskData.title } : task
+          task.id === editTaskId
+            ? { ...task, title: updatedTaskData.title }
+            : task
         )
       );
-      setInputValue('');
+      setInputValue("");
       setEditTaskId(null);
-      toast.success('Task updated successfully');
+      toast.success("Task updated successfully");
     } catch (error) {
-      console.log('Error updating task:', error);
-      toast.error('Error updating task');
+      console.log("Error updating task:", error);
+      toast.error("Error updating task");
     }
   };
 
@@ -139,11 +149,11 @@ const TodoList = () => {
 
   // Filter tasks based on the selected filter
   const filteredTasks = tasks.filter((task) => {
-    if (filter === 'all') {
+    if (filter === "all") {
       return true;
-    } else if (filter === 'completed') {
+    } else if (filter === "completed") {
       return task.completed;
-    } else if (filter === 'uncompleted') {
+    } else if (filter === "uncompleted") {
       return !task.completed;
     }
     return true;
@@ -173,21 +183,33 @@ const TodoList = () => {
             value={inputValue}
             onChange={handleInputChange}
           />
-          <button id="btn" onClick={editTaskId ? handleUpdateTask : handleAddTask}>
-            {editTaskId ? 'Update' : 'Add'}
+          <button
+            id="btn"
+            onClick={editTaskId ? handleUpdateTask : handleAddTask}
+          >
+            {editTaskId ? "Update" : "Add"}
           </button>
         </div>
 
         <div className="mid">
-          <i className="fas fa-check-double"></i>
           <p id="complete-all" onClick={handleCompleteAll}>
+            <i className="fas fa-check-double"></i>
             Complete all tasks
           </p>
           <p id="clear-all" onClick={handleClearCompleted}>
+            <i class="fa fa-trash" aria-hidden="true"></i>
             Delete comp tasks
           </p>
         </div>
-
+        <div
+          style={{
+            width: "100%",
+            height: "1px",
+            backgroundColor: "#ddd",
+            marginTop: "5px",
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+          }}
+        ></div>
         <ul id="list">
           {filteredTasks.map((task) => (
             <li key={task.id}>
@@ -201,18 +223,30 @@ const TodoList = () => {
               />
               <label htmlFor={`task-${task.id}`}>{task.title}</label>
               <div>
-                <img
-                  src="https://cdn-icons-png.flaticon.com/128/1159/1159633.png"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="#388E3C"
                   className="edit"
                   data-id={task.id}
                   onClick={() => handleEditTask(task.id)}
-                />
-                <img
-                  src="https://cdn-icons-png.flaticon.com/128/3096/3096673.png"
+                >
+                  <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="red"
                   className="delete"
                   data-id={task.id}
                   onClick={() => handleDeleteTask(task.id)}
-                />
+                >
+                  <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+                </svg>
               </div>
             </li>
           ))}
@@ -222,13 +256,21 @@ const TodoList = () => {
           <div className="dropdown">
             <button className="dropbtn">Filter</button>
             <div className="dropdown-content">
-              <a href="#" id="all" onClick={() => handleFilterChange('all')}>
+              <a href="#" id="all" onClick={() => handleFilterChange("all")}>
                 All
               </a>
-              <a href="#" id="rem" onClick={() => handleFilterChange('uncompleted')}>
+              <a
+                href="#"
+                id="rem"
+                onClick={() => handleFilterChange("uncompleted")}
+              >
                 Uncompleted
               </a>
-              <a href="#" id="com" onClick={() => handleFilterChange('completed')}>
+              <a
+                href="#"
+                id="com"
+                onClick={() => handleFilterChange("completed")}
+              >
                 Completed
               </a>
             </div>
@@ -236,7 +278,10 @@ const TodoList = () => {
 
           <div className="completed-task">
             <p>
-              Completed: <span id="c-count">{tasks.filter((task) => task.completed).length}</span>
+              Completed:{" "}
+              <span id="c-count">
+                {tasks.filter((task) => task.completed).length}
+              </span>
             </p>
           </div>
           <div className="remaining-task">
